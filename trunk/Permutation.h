4d419f706c7c4c95b740699bd6c7878725e4c47f
ennetws <ennetws@b89debb9-ee29-8e8f-1b88-6f4c7e002cbc>
2012-11-13 23:10:24 +0000

    Final submission.

---
+++ trunk/Permutation.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+struct Permutation{
+
+	Permutation(){}
+
+	void load(string filename, int numShingles, int & numHashFuncs)
+	{
+
+		ifstream inF(filename.c_str(), ios::in);
+
+
+		inF >> numHashFuncs; 
+
+		PT.resize(numHashFuncs, vector<int>(numShingles, -1));
+
+		for(int i = 0; i < numHashFuncs; i++){
+			for(int x = 0; x < numShingles; x++){
+				inF >> PT[i][x];
+			}
+		}
+	}
+	
+	vector< vector<int> > PT;
+
+	int hash(int i, int x)
+	{
+		return PT[i][x];
+	}
+
+};
